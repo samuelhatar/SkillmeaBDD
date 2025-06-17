4363a59f70c28db86da158cb40889f185c5696ef
samuelhatar <samuel.hatar@gmail.com>
2025-06-17 19:52:58 +0200

    init commit

---
+++ src/main/java/PokusBdd.java
@@ -0,0 +1,31 @@
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class PokusBdd {
+    @Given("nastavenie pociatocnych podmienok")
+    public void nastaveniePociatocnychPodmienok() {
+
+    }
+
+    @And("dalsi nastavovaci krok")
+    public void dalsiNastavovaciKrok() {
+    }
+
+    @When("toto je niaka akcia")
+    public void totoJeNiakaAkcia() {
+    }
+
+    @And("toto je dalsia akcia")
+    public void totoJeDalsiaAkcia() {
+    }
+
+    @Then("overenie vysledku")
+    public void overenieVysledku() {
+    }
+
+    @And("dalsie overenie vysledku")
+    public void dalsieOverenieVysledku() {
+    }
+}
---
+++ src/main/resources/pokus.feature
@@ -0,0 +1,16 @@
+Feature: pokus
+
+  #Toto je komentar
+
+  Scenario: Pokusny test 1
+    Given nastavenie pociatocnych podmienok
+    And dalsi nastavovaci krok
+    When toto je niaka akcia
+    And toto je dalsia akcia
+    Then overenie vysledku
+    And dalsie overenie vysledku
+
+    Scenario: pokusny test 2
+      Given nastavenie pociatocnych podmienok
+      When toto je dalsia akcia
+      Then dalsie overenie vysledku
