2d1b9930b9d8687ef3d1151fafaab9942c6d735f
samuelhatar <samuel.hatar@gmail.com>
2025-06-19 19:01:18 +0200

    init commit

--- src/main/java/BankTest.java
+++ src/main/java/BankTest.java
@@ -1,12 +1,19 @@
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import product.BankAccount;
 
 public class BankTest {
+
+    BankAccount firstBankAccount;
+
+
     @Given("Bank account with {int} euro")
-    public void bankAccountWithEuro(int arg0) {
+    public void bankAccountWithEuro(int initialBalance) {
+        firstBankAccount = new initialBalance (initialBalance);
     }
 
     @Then("A user account balance is {int} euro")
-    public void aUserAccountBalanceIsEuro(int arg0) {
+    public void aUserAccountBalanceIsEuro(int expectedBalance) {
+
     }
 }
