b420b0476939a35983095c10bf622a7ef261cbf1
samuelhatar <samuel.hatar@gmail.com>
2025-06-19 18:48:51 +0200

    init commit

---
+++ src/main/java/BankTest.java
@@ -0,0 +1,12 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class BankTest {
+    @Given("Bank account with {int} euro")
+    public void bankAccountWithEuro(int arg0) {
+    }
+
+    @Then("A user account balance is {int} euro")
+    public void aUserAccountBalanceIsEuro(int arg0) {
+    }
+}
---
+++ src/main/resources/Bank.feature
@@ -0,0 +1,5 @@
+Feature: Bank
+
+  Scenario: Bank account balance
+    Given Bank account with 1000 euro
+    Then A user account balance is 1000 euro
